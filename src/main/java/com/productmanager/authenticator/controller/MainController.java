d1c5312202e19735fee957902716dbbff4fcefb2
daham.embuldeniya@axiatadigitallabs.com <daham.embuldeniya@axiatadigitallabs.com>
2022-04-11 13:56:54 +0530

    init

---
+++ src/main/java/com/productmanager/authenticator/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.productmanager.authenticator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public ResponseEntity getUser(){
+        return ResponseEntity.ok().body("received");
+    }
+}
